8ba8cd25eea1def09108f9686e8575062b6f27f8
dacthu784 <125370292+dacthu784@users.noreply.github.com>
2024-04-17 15:46:45 +0700

    hh

--- Quanlyquanan/obj/Debug/net7.0-windows/Quanlyquanan.AssemblyInfo.cs
+++ Quanlyquanan/obj/Debug/net7.0-windows/Quanlyquanan.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Quanlyquanan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8b3bb99d4779f5031d081b8f58cc0b07185d1c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Quanlyquanan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Quanlyquanan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
